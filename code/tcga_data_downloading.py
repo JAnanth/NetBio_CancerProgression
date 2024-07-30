6ce999c47940803899a75e3392338a166294836f
JAnanth <Jay$1234>
2024-07-30 10:38:24 -0700

    testing pycharm

--- code/tcga_data_downloading.py
+++ code/tcga_data_downloading.py
@@ -1,9 +1,7 @@
 import os
 import subprocess
-import pandas as pd
 from os import getcwd
-# af
-print(getcwd())
+import pandas as pd
 
 # Path to gdc-client executable
 gdc_client_path = getcwd() + '/data/manifests/gdc-client'
@@ -22,6 +20,7 @@ output_dir = 'tcga_data'
 # Create output directory if it doesn't exist
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-output_dir
+print(output_dir)
 # Download the data
 download_data(gdc_client_path, manifest_path, output_dir)
+print(getcwd())
