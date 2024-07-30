fd0cffe5835b40eeae56c4abd94154fafba11006
JAnanth <64097947+JAnanth@users.noreply.github.com>
2024-07-29 20:52:09 -0700

    Add files via upload

---
+++ tcga_data_downloading.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import pandas as pd
+from os import getcwd
+
+# Path to gdc-client executable
+gdc_client_path = getcwd() + '/gdc-client'
+
+# Path to your manifest file
+manifest_path = getcwd() + '/gdc_manifest.2024-07-30.txt'
+
+# Function to download data using gdc-client
+def download_data(gdc_client_path, manifest_path, output_dir):
+    command = [gdc_client_path, 'download', '-m', manifest_path, '-d', output_dir]
+    subprocess.run(command, check=True)
+
+# Output directory for downloaded files
+output_dir = 'tcga_data'
+
+# Create output directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+output_dir
+# Download the data
+download_data(gdc_client_path, manifest_path, output_dir)
