e34d8ee47393504fa9196c7ca124ed22dc0506c0
JAnanth <Jay$1234>
2024-07-30 20:24:16 -0700

    updating data_loading code for both methylation and expression data

--- code/tcga_data_downloading.py
+++ code/tcga_data_downloading.py
@@ -1,26 +1,42 @@
 import os
 import subprocess
 from os import getcwd
+import subprocess
 import pandas as pd
 
 # Path to gdc-client executable
 gdc_client_path = getcwd() + '/data/manifests/gdc-client'
 
 # Path to your manifest file
-manifest_path = getcwd() + '/data/manifests/gdc_manifest.2024-07-30.txt'
+manifest_path = getcwd() + '/data/manifests/gdc_ge_manifest.txt'
 
-# Function to download data using gdc-client
 def download_data(gdc_client_path, manifest_path, output_dir):
     command = [gdc_client_path, 'download', '-m', manifest_path, '-d', output_dir]
-    subprocess.run(command, check=True)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Print the output in real-time
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+
+    # Check for errors
+    stderr = process.stderr.read()
+    if stderr:
+        print(stderr)
+
+    # Ensure the process completed successfully
+    if process.returncode != 0:
+        raise subprocess.CalledProcessError(process.returncode, command)
 
 # Output directory for downloaded files
-output_dir = 'tcga_data'
+output_dir = getcwd() + '/data/tcga_ge_data'
 
 # Create output directory if it doesn't exist
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-print(output_dir)
+
 # Download the data
 download_data(gdc_client_path, manifest_path, output_dir)
-print(getcwd())
--- code/txt_to_df.py
+++ code/txt_to_df.py
@@ -16,7 +16,7 @@ def move_txt_files(source_folder, destination_folder):
         print(f"Processing directory: {subdir}")
 
         for file in files:
-            if file.endswith('.txt'):
+            if file.endswith('.tsv'):
                 source_path = os.path.join(subdir, file)
                 destination_path = os.path.join(destination_folder, file)
 
@@ -38,16 +38,75 @@ def move_txt_files(source_folder, destination_folder):
                 print(f"Moved: {source_path} -> {destination_path}")
 
 
-source_folder = getcwd() + '/data/tcga_data'
-destination_folder = getcwd() + '/data/processed'  # Replace with the desired output folder path
+source_folder = getcwd() + '/data/tcga_ge_data'
+destination_folder = getcwd() + '/data/processed/tcga_ge_processed'  # Replace with the desired output folder path
 
 move_txt_files(source_folder, destination_folder)
 
+def convert_txt_to_df(txts_path):
+    count = 0
+    for name in os.listdir(txts_path):
+        temp_file_path = f'{txts_path}/{name}'
+        df = pd.read_csv(temp_file_path, names=['site', 'beta_val'], sep='	')
+        count += 1
+        df.to_csv(f'{getcwd()}/data/processed/ge_dfs/df{count}.csv', index=False)
+    return count
+
 txts_path = getcwd() + '/data/processed/tcga_methyl_processed'
-count = 0
-for name in os.listdir(txts_path):
-    temp_file_path = f'{txts_path}/{name}'
-    df = pd.read_csv(temp_file_path, names=['site', 'beta_val'], sep='	')
-    count += 1
-    df.to_csv(f'{getcwd()}/data/processed/methyl_dfs/df{count}.csv')
+count = convert_txt_to_df(txts_path)
 print(count)
+# Combine the dataframes
+def read_dataframes(directory_path):
+    # List to hold all dataframes
+    dfs = []
+    directory_path = dir_path
+    # Iterate over all files in the directory
+    for filename in os.listdir(directory_path):
+        if filename.endswith('.csv'):
+            # Read the CSV file into a dataframe
+            file_path = os.path.join(directory_path, filename)
+            try:
+                df = pd.read_csv(file_path)
+                if df.shape == (27578, 2) and df['site'][0] == 'cg00000292':
+                    dfs.append(df)
+                # Ensure the dataframe has the required columns
+                # if 'site' in df.columns and 'beta_val' in df.columns:
+                    # Set the 'site' column as index
+                    # df = df.set_index('site')
+                # else:
+                #     print(f"File {file_path} does not have the required columns.")
+            except UnicodeDecodeError as e:
+                print(f"Encoding error in file {file_path}: {e}")
+                continue
+
+    # Debug: print the shapes of all dataframes
+    for i, df in enumerate(dfs):
+        print(f"DataFrame {i+1} shape: {df.shape}")
+
+    return dfs
+
+def combined_dataframes(directory_path):
+    dfs = read_dataframes(directory_path)
+    # Ensure all dataframes have the same site values
+    site_values = dfs[0]['site']
+
+    # Initialize a list to hold the beta_val columns
+    beta_values_list = []
+
+    for df in dfs:
+        # Set the 'site' column as index and get the beta_val column
+        beta_values = df.set_index('site')['beta_val']
+        beta_values_list.append(beta_values)
+
+    # Concatenate the beta_val columns along the columns axis
+    combined_df = pd.concat(beta_values_list, axis=1)
+    # Set the site column as the index of the final dataframe
+    combined_df.index = site_values
+    # Rename the columns to differentiate the beta_val columns
+    combined_df.columns = [f'patient_beta_{i + 1}' for i in range(len(dfs))]
+    return combined_df
+
+
+dir_path = getcwd() + '/data/processed/methyl_dfs'
+combined_df = combined_dataframes(dir_path)
+combined_df.to_csv(getcwd() + '/data/processed/methyl_full_df.csv', index=False)
